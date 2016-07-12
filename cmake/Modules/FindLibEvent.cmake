8b3e0833d35cee979d85193012d780288c8241a7
zhibiao.pan <zhibiao.pan@bitmain.com>
2016-07-12 17:36:10 +0800

    update libevent

--- cmake/Modules/FindLibEvent.cmake
+++ cmake/Modules/FindLibEvent.cmake
@@ -2,27 +2,36 @@
 # This module defines
 # LIBEVENT_INCLUDE_DIR, where to find LibEvent headers
 # LIBEVENT_LIB, LibEvent libraries
-# LIBEVENT_FOUND, If false, do not try to use libevent
+# LibEvent_FOUND, If false, do not try to use libevent
 
-find_path(
-  LIBEVENT_INCLUDE_DIR
-  event2/event.h
-  HINTS
-  ${LIBEVENT_DIR}
-  $ENV{LIBEVENT_DIR}
-  /usr
-  /opt
-  PATH_SUFFIXES include )
+set(LibEvent_EXTRA_PREFIXES /usr/local /opt/local "$ENV{HOME}")
+foreach(prefix ${LibEvent_EXTRA_PREFIXES})
+  list(APPEND LibEvent_INCLUDE_PATHS "${prefix}/include")
+  list(APPEND LibEvent_LIB_PATHS "${prefix}/lib")
+endforeach()
 
-find_library(
-  LIBEVENT_LIB
-  NAMES event
-  HINTS
-  ${LIBEVENT_DIR}
-  $ENV{LIBEVENT_DIR}
-  /usr
-  /opt
-  PATH_SUFFIXES lib )
+find_path(LIBEVENT_INCLUDE_DIR event.h PATHS ${LibEvent_INCLUDE_PATHS})
+find_library(LIBEVENT_LIB NAMES event PATHS ${LibEvent_LIB_PATHS})
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibEvent DEFAULT_MSG LIBEVENT_LIB LIBEVENT_INCLUDE_DIR)
+if (LIBEVENT_LIB AND LIBEVENT_INCLUDE_DIR)
+  set(LibEvent_FOUND TRUE)
+  set(LIBEVENT_LIB ${LIBEVENT_LIB})
+else ()
+  set(LibEvent_FOUND FALSE)
+endif ()
+
+if (LibEvent_FOUND)
+  if (NOT LibEvent_FIND_QUIETLY)
+    message(STATUS "Found libevent: ${LIBEVENT_LIB}")
+  endif ()
+else ()
+  if (LibEvent_FIND_REQUIRED)
+    message(FATAL_ERROR "Could NOT find libevent.")
+  endif ()
+  message(STATUS "libevent NOT found.")
+endif ()
+
+mark_as_advanced(
+    LIBEVENT_LIB
+    LIBEVENT_INCLUDE_DIR
+  )
