aad92c9279da4aa14093f50589b95c028905bb2b
Kevin Pan <zhibiao.pan@bitmain.com>
2016-09-26 22:01:06 +0800

    remove unused function

--- src/Utils.cc
+++ src/Utils.cc
@@ -111,57 +111,6 @@ void Bin2HexR(const vector<char> &in, string &str) {
   Bin2Hex(r, str);
 }
 
-//static bool _DecodeBinTx(CTransaction& tx, vector<unsigned char> &data) {
-//  CDataStream ssData(data, SER_NETWORK, BITCOIN_PROTOCOL_VERSION);
-//  try {
-//    ssData >> tx;
-//  }
-//  catch (const std::exception &) {
-//    return false;
-//  }
-//  return true;
-//}
-//
-//bool DecodeBinTx(CTransaction& tx, const unsigned char *data, size_t len) {
-//  vector<unsigned char> txData(data, data + len);
-//  return _DecodeBinTx(tx, txData);
-//}
-//
-//bool DecodeHexTx(CTransaction& tx, const std::string& strHexTx)
-//{
-//  if (!IsHex(strHexTx))
-//    return false;
-//
-//  vector<unsigned char> txData(ParseHex(strHexTx));
-//  return _DecodeBinTx(tx, txData);
-//}
-//
-//
-//static bool _DecodeBinBlk(CBlock& block, vector<unsigned char> &data) {
-//  CDataStream ssBlock(data, SER_NETWORK, BITCOIN_PROTOCOL_VERSION);
-//  try {
-//    ssBlock >> block;
-//  }
-//  catch (const std::exception &) {
-//    return false;
-//  }
-//  return true;
-//}
-//
-//bool DecodeBinBlk(CBlock& block, const unsigned char *data, size_t len) {
-//  vector<unsigned char> blkData(data, data + len);
-//  return _DecodeBinBlk(block, blkData);
-//}
-//
-//bool DecodeHexBlk(CBlock& block, const std::string& strHexBlk)
-//{
-//  if (!IsHex(strHexBlk))
-//    return false;
-//
-//  std::vector<unsigned char> blkData(ParseHex(strHexBlk));
-//  return _DecodeBinBlk(block, blkData);
-//}
-
 //  Receive 0MQ string from socket and convert into string
 std::string s_recv (zmq::socket_t & socket) {
   zmq::message_t message;
