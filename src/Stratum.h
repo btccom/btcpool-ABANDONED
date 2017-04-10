db15d87aa92530cff6feb33d363bd82a22f4cd58
Martín Medina <tinnmedina@gmail.com>
2017-04-10 17:51:48 -0300

    Change representation of some fields on RskSolvedShareData object — missing file from previous commit

--- src/Stratum.h
+++ src/Stratum.h
@@ -73,13 +73,16 @@ public:
   int32_t  height_;
   uint8_t  header80_[80];
   char     workerFullName_[40];  // <UserName>.<WorkerName>
-  string   feesForMiner_;
-  string   rpcAddress_;
-  string   rpcUserPwd_;
+  char     feesForMiner_[80];
+  char     rpcAddress_[80];
+  char     rpcUserPwd_[80];
 
   RskSolvedShareData(): jobId_(0), workerId_(0), userId_(0), height_(0) {
     memset(header80_,       0, sizeof(header80_));
     memset(workerFullName_, 0, sizeof(workerFullName_));
+    memset(feesForMiner_, 0, sizeof(feesForMiner_));
+    memset(rpcAddress_, 0, sizeof(rpcAddress_));
+    memset(rpcUserPwd_, 0, sizeof(rpcUserPwd_));
   }
 };
 
