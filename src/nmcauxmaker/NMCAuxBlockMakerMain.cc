c8059bd6287564a3d29f5fe2ab2dadc2626ea9d0
zhibiao.pan <zhibiao.pan@bitmain.com>
2017-05-09 19:08:30 +0800

    Update to create/submit aux block

--- src/nmcauxmaker/NMCAuxBlockMakerMain.cc
+++ src/nmcauxmaker/NMCAuxBlockMakerMain.cc
@@ -118,13 +118,15 @@ int main(int argc, char **argv) {
   cfg.lookupValue("nmcauxmaker.rpcinterval", rpcCallInterval);
   string fileLastRpcCallTime;
   cfg.lookupValue("nmcauxmaker.file_last_rpc_call_time", fileLastRpcCallTime);
+  string coinbaseAddress;
+  cfg.lookupValue("nmcauxmaker.payout_address", coinbaseAddress);
 
   gNMCAuxBlockMaker = new NMCAuxBlockMaker(cfg.lookup("namecoind.zmq_addr"),
                                            cfg.lookup("namecoind.rpc_addr"),
                                            cfg.lookup("namecoind.rpc_userpwd"),
                                            cfg.lookup("kafka.brokers"),
                                            rpcCallInterval, fileLastRpcCallTime,
-                                           isCheckZmq);
+                                           isCheckZmq, coinbaseAddress);
 
   try {
     if (!gNMCAuxBlockMaker->init()) {
