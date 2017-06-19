f2ea37e7a74dfc24d120cba5c7bd748eb71a5ea1
zhibiao.pan <zhibiao.pan@bitmain.com>
2017-06-19 15:37:21 +0800

    add prefix for kafka topic

--- src/Kafka.h
+++ src/Kafka.h
@@ -28,14 +28,11 @@
 
 #include <librdkafka/rdkafka.h>
 
-#define KAFKA_TOPIC_RAWGBT         "RawGbt"
-#define KAFKA_TOPIC_STRATUM_JOB    "StratumJob"
-#define KAFKA_TOPIC_SOLVED_SHARE   "SolvedShare"
-#define KAFKA_TOPIC_SHARE_LOG      "ShareLog"
-#define KAFKA_TOPIC_COMMON_EVENTS  "CommonEvents"
-
-#define KAFKA_TOPIC_NMC_AUXBLOCK      "NMCAuxBlock"
-#define KAFKA_TOPIC_NMC_SOLVED_SHARE  "NMCSolvedShare"
+#define KAFKA_TOPIC_RAWGBT         "ZEC_RawGbt"
+#define KAFKA_TOPIC_STRATUM_JOB    "ZEC_StratumJob"
+#define KAFKA_TOPIC_SOLVED_SHARE   "ZEC_SolvedShare"
+#define KAFKA_TOPIC_SHARE_LOG      "ZEC_ShareLog"
+#define KAFKA_TOPIC_COMMON_EVENTS  "ZEC_CommonEvents"
 
 
 ///////////////////////////////// KafkaConsumer ////////////////////////////////
