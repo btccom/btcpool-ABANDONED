270e9d5133daab0c8aec2e5deb7952a6428a94d2
YihaoPeng <yihao.peng@bitmain.com>
2018-03-01 12:13:33 +0800

    edit a comment.

--- src/RedisConnection.h
+++ src/RedisConnection.h
@@ -30,14 +30,14 @@
 using namespace std;
 
 /* 
-* <redis/read.h>
-* possible values redisReply->type:
+* Possible values of `redisReply->type`:
 *   REDIS_REPLY_STRING    1
 *   REDIS_REPLY_ARRAY     2
 *   REDIS_REPLY_INTEGER   3
 *   REDIS_REPLY_NIL       4
 *   REDIS_REPLY_STATUS    5
 *   REDIS_REPLY_ERROR     6
+* From: <hiredis/read.h>
 */
 
 /////////////////////////////// RedisResult ///////////////////////////////
