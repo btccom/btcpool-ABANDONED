4827ae706b243414a284760ace26414ceed5376c
fatrat1117 <fatrat1117@gmail.com>
2018-03-08 17:17:58 +0800

    define block information

--- src/JobMaker.h
+++ src/JobMaker.h
@@ -115,6 +115,49 @@ public:
   virtual void run();
 };
 
+// number: QUANTITY - the block number. null when its pending block.
+// hash: DATA, 32 Bytes - hash of the block. null when its pending block.
+// parentHash: DATA, 32 Bytes - hash of the parent block.
+// nonce: DATA, 8 Bytes - hash of the generated proof-of-work. null when its pending block.
+// sha3Uncles: DATA, 32 Bytes - SHA3 of the uncles data in the block.
+// logsBloom: DATA, 256 Bytes - the bloom filter for the logs of the block. null when its pending block.
+// transactionsRoot: DATA, 32 Bytes - the root of the transaction trie of the block.
+// stateRoot: DATA, 32 Bytes - the root of the final state trie of the block.
+// receiptsRoot: DATA, 32 Bytes - the root of the receipts trie of the block.
+// miner: DATA, 20 Bytes - the address of the beneficiary to whom the mining rewards were given.
+// difficulty: QUANTITY - integer of the difficulty for this block.
+// totalDifficulty: QUANTITY - integer of the total difficulty of the chain until this block.
+// extraData: DATA - the "extra data" field of this block.
+// size: QUANTITY - integer the size of this block in bytes.
+// gasLimit: QUANTITY - the maximum gas allowed in this block.
+// gasUsed: QUANTITY - the total used gas by all transactions in this block.
+// timestamp: QUANTITY - the unix timestamp for when the block was collated.
+// transactions: Array - Array of transaction objects, or 32 Bytes transaction hashes depending on the last given parameter.
+// uncles: Array - Array of uncle hashes.
+
+class BlockHeaderEth
+{
+  public:
+    uint64_t number_;
+    uint64_t difficulty_;
+    uint64_t totalDifficulty_;
+    uint32_t gasLimit_;
+    uint32_t gasUsed_;
+    uint32_t timestamp_;
+    uint32_t size_;
+    string extraData_;
+    string hash_;
+    string logsBloom_;
+    string miner_;
+    string mixHash_;
+    string nonce_;
+    string parentHash_;
+    string receiptsRoot_;
+    string sha3Uncles_;
+    string stateRoot_;
+    string transactionsRoot_;
+};
+
 class JobMakerEth : public JobMaker
 {
   virtual RskWork* createWork();
