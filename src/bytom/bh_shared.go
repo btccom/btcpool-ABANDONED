5bc4c47a2ada4987ff87118b826d886e9d0b35af
fatrat1117 <fatrat1117@gmail.com>
2018-05-04 17:23:34 +0800

    return all information needed for stratum

--- src/bytom/bh_shared.go
+++ src/bytom/bh_shared.go
@@ -6,10 +6,13 @@ import (
 )
 
 //export DecodeHeaderString
-func DecodeHeaderString(text []byte) (uint64, uint64, uint64, uint64) {
-	bh := &types.BlockHeader{}
+func DecodeHeaderString(text []byte) (uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64) {
+	bh := types.BlockHeader{}
 	bh.UnmarshalText(text)
-	return bh.Version, bh.Height, bh.Timestamp, bh.Bits
+	return bh.Version, bh.Height, bh.Timestamp, bh.Bits,
+		bh.PreviousBlockHash.GetV0(), bh.PreviousBlockHash.GetV1(), bh.PreviousBlockHash.GetV2(), bh.PreviousBlockHash.GetV3(),
+		bh.BlockCommitment.TransactionsMerkleRoot.GetV0(), bh.BlockCommitment.TransactionsMerkleRoot.GetV1(), bh.BlockCommitment.TransactionsMerkleRoot.GetV2(), bh.BlockCommitment.TransactionsMerkleRoot.GetV3(),
+		bh.BlockCommitment.TransactionStatusHash.GetV0(), bh.BlockCommitment.TransactionStatusHash.GetV1(), bh.BlockCommitment.TransactionStatusHash.GetV2(), bh.BlockCommitment.TransactionStatusHash.GetV3()
 }
 
 func main() {
