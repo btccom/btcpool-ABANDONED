15eac1c1d2d87cef78b24a0fe51d10a4106ded5a
gbc_sg <ganibc@gmail.com>
2018-05-17 16:41:49 +0800

    add sql file for poolwatcher receive log

---
+++ install/poolwatcher_receive_log.sql
@@ -0,0 +1,10 @@
+TABLE `poolwatcherreceivelog` (
+  `poolname` varchar(25) NOT NULL,
+  `poolhost` varchar(25) NOT NULL,
+  `blockhash` varchar(64) NOT NULL,
+  `blockheight` int(11) NOT NULL,
+  `receivetime` datetime(3) NOT NULL,
+  PRIMARY KEY (`poolname`,`blockhash`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+order by blockheight, receivetime
