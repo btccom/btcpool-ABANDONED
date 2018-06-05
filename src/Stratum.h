e3263bd883739337cf46b41f1ffb027eec438c32
YihaoPeng <yihao.peng@bitmain.com>
2018-06-05 10:51:40 +0800

    add a IPv4/IPv6 compatible address structure.

--- src/Stratum.h
+++ src/Stratum.h
@@ -83,6 +83,88 @@ public:
   }
 };
 
+///////////////////////////////////// IPv4/IPv6 compatible address structure ////////////////////////////////////
+union IpAddress {
+  // all datas are big endian
+  uint8_t  addrByte[16];
+  uint16_t addrShort[8];
+  uint32_t addrLong[4];
+  // use addrIpv4[3] to store the IPv4 addr
+  struct in_addr addrIpv4[4];
+  struct in6_addr addrIpv6;
+
+  // memory mapping:
+  // addrByte  | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|13|14|15|
+  // addrShort |  0  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |
+  // addrLong  |     0     |     1     |     2     |     3     |
+  // addrIpv4  | don't use | don't use | don't use |     3     |
+  // addrIpv6  |                      all                      |
+
+  bool fromString(const string &ipStr) {
+    if (isIpv4(ipStr)) {
+      addrLong[0] = 0;
+      addrLong[1] = 0;
+      addrLong[2] = 0;
+      return inet_pton(AF_INET, ipStr.c_str(), (void *)&addrIpv4[3]);
+    }
+    else {
+      return inet_pton(AF_INET, ipStr.c_str(), (void *)&addrIpv6);
+    }
+  }
+
+  string toString() {
+    const char *pStr;
+
+    if (isIpv4()) {
+      char str[INET_ADDRSTRLEN];  
+      pStr = inet_ntop(AF_INET, (void *)&(addrIpv4[3]), str, sizeof(str));
+    }
+    else {
+      char str[INET6_ADDRSTRLEN];  
+      pStr = inet_ntop(AF_INET6, &addrIpv6, str, sizeof(str));
+    }
+
+    return string(pStr);
+  }
+
+  void fromInAddr(const struct in_addr &inAddr) {
+    addrLong[0] = 0;
+    addrLong[1] = 0;
+    addrLong[2] = 0;
+    addrIpv4[3] = inAddr;
+  }
+
+  void fromInAddr(const struct in6_addr &inAddr) {
+    addrIpv6 = inAddr;
+  }
+
+  bool isIpv4() {
+    if (addrLong[0] == 0 && addrLong[1] == 0) {
+      // IPv4 compatible address
+      // ::w.x.y.z
+      if (addrLong[2] == 0) {
+        return true;
+      }
+      // IPv4 mapping address
+      // ::ffff:w.x.y.z
+      if (addrShort[4] == 0 && addrShort[5] == 0xffff) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static bool isIpv4(const string &ipStr) {
+    if (ipStr.find(':') == ipStr.npos) {
+      return true;
+    }
+    return false;
+  }
+};
+
+// IpAddress should be 16 bytes
+static_assert(sizeof(IpAddress) == 16, "union IpAddress should not large than 16 bytes");
+
 ///////////////////////////////////// Share ////////////////////////////////////
 class Share
 {
