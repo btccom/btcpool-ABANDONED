ea0d97bb22d3b644f7a79961f5678cb3d7d66771
gani <ganibc@gmail.com>
2018-06-05 10:13:04 +0800

    add copy btcpool executables from build folder

--- install/supervisord/replace_root_dir.sh
+++ install/supervisord/replace_root_dir.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-#default root dir is /work/btcpool
-default_dir="/work/btcpool"
+# script to help replace btcpool supervisor's .conf file.
+# init_package_folder allow us to put installation folder other than /work/btcpool/build, so this script is to help replacing the installation folder
+ 
+cur_dir=`pwd`
+echo $cur_dir
+default_dir="/work/btcpool/build"
 new_root_dir=$1
 
 if [ -d "$new_root_dir" ]; then
@@ -13,12 +17,16 @@ if [ -d "$new_root_dir" ]; then
     escaped_new_root_dir=${new_root_dir//\//\\/}
 
     syntax="s/${escaped_default_dir}/${escaped_new_root_dir}/g"
-    sed -i -e ${syntax} blkmaker.conf
-    sed -i -e ${syntax} gbtmaker.conf
-    sed -i -e ${syntax} gwmaker.conf
-    sed -i -e ${syntax} jobmaker.conf
-    sed -i -e ${syntax} poolwatcher.conf
-    sed -i -e ${syntax} sserver.conf
+    sed -i -e ${syntax} ${cur_dir}/blkmaker.conf
+    sed -i -e ${syntax} ${cur_dir}/gbtmaker.conf
+    sed -i -e ${syntax} ${cur_dir}/gwmaker.conf
+    sed -i -e ${syntax} ${cur_dir}/jobmaker.conf
+    sed -i -e ${syntax} ${cur_dir}/nmcauxmaker.conf
+    sed -i -e ${syntax} ${cur_dir}/poolwatcher.conf
+    sed -i -e ${syntax} ${cur_dir}/sharelogger.conf
+    sed -i -e ${syntax} ${cur_dir}/slparser.conf
+    sed -i -e ${syntax} ${cur_dir}/sserver.conf
+    sed -i -e ${syntax} ${cur_dir}/statshttpd.conf
 
 else
     echo "$new_root_dir not exist"
