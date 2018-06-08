558a4a679334dc19c4ed975f29559c6c44c88494
YihaoPeng <yihao.peng@bitmain.com>
2018-06-08 16:35:48 +0800

    fix GetBlockSubsidy for the 63th halvings (-2 returned before).

--- src/BitcoinUtils.cc
+++ src/BitcoinUtils.cc
@@ -44,7 +44,7 @@ AMOUNT_TYPE GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParam
   AMOUNT_TYPE nSubsidy = AMOUNT_TYPE(50 * COIN_TO_SATOSHIS);
 
   // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-  nSubsidy = nSubsidy / (1 << halvings);
+  nSubsidy >>= halvings; // this line is secure, it copied from bitcoin's validation.cpp
   return nSubsidy;
 }
 
