d4813cb42a03d27fddaeb7887019b74f6c55dc26
gani <ganibc@gmail.com>
2018-07-23 14:07:18 +0800

    clean unused header files in ShareLogger.h

--- src/ShareLogger.h
+++ src/ShareLogger.h
@@ -26,20 +26,10 @@
 
 #include "Common.h"
 #include "Kafka.h"
-
-#include "eth/StratumEth.h"
+#include "Utils.h"
 
 #include "zlibstream/zstr.hpp"
-
-#include <event2/event.h>
-#include <event2/http.h>
-#include <event2/buffer.h>
-#include <event2/util.h>
-#include <event2/keyvalq_struct.h>
-
-#include <string.h>
-#include <pthread.h>
-#include <memory>
+#include <glog/logging.h>
 
 
 //////////////////////////////  ShareLogWriter  ///////////////////////////////
