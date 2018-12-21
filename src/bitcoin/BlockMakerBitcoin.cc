71313c144a9c61b2c3843204553a44a05d2b7629
YihaoPeng <yihao.peng@bitmain.com>
2018-12-21 18:06:56 +0800

    blkmaker: simplified code; add `found_aux_block_table` to blkmaker.cfg

--- src/bitcoin/BlockMakerBitcoin.cc
+++ src/bitcoin/BlockMakerBitcoin.cc
@@ -454,22 +454,13 @@ void BlockMakerBitcoin::_submitNamecoinBlockThread(const string &auxBlockHash,
   {
     const string nowStr = date("%F %T");
     string sql;
-    if(def()->foundAuxBlockTable_.empty() || def()->foundAuxBlockTable_ == " ") {
-      sql = Strings::Format("INSERT INTO `found_nmc_blocks` "
+    sql = Strings::Format("INSERT INTO `%s` "
                           " (`bitcoin_block_hash`,`aux_block_hash`,"
                           "  `aux_pow`,`created_at`) "
                           " VALUES (\"%s\",\"%s\",\"%s\",\"%s\"); ",
+                          def()->foundAuxBlockTable_.empty() ? "found_nmc_blocks" : def()->foundAuxBlockTable_.c_str(),
                           bitcoinBlockHash.c_str(),
                           auxBlockHash.c_str(), auxPow.c_str(), nowStr.c_str());
-    } else {
-      sql = Strings::Format("INSERT INTO `%s` "
-                          " (`bitcoin_block_hash`,`aux_block_hash`,"
-                          "  `aux_pow`,`created_at`) "
-                          " VALUES (\"%s\",\"%s\",\"%s\",\"%s\"); ",
-                          def()->foundAuxBlockTable_.c_str(),
-                          bitcoinBlockHash.c_str(),
-                          auxBlockHash.c_str(), auxPow.c_str(), nowStr.c_str());
-    }
     // try connect to DB
     MySQLConnection db(poolDB_);
     for (size_t i = 0; i < 3; i++) {
