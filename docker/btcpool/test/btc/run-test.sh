06d76e207c369e7ddd62058633af1f65c594c5ce
SwimmingTiger <hu60.cn@gmail.com>
2019-05-03 18:22:23 +0800

    autotest: use APT_MIRROR_URL; docker deploy: enable BUILD_TESTING by default

--- docker/btcpool/test/btc/run-test.sh
+++ docker/btcpool/test/btc/run-test.sh
@@ -4,8 +4,17 @@
 export BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
 export STDOUT="/tmp/btcpool-test-btc-stdout.log"
 export LOGFILE="/tmp/btcpool-test-btc-result.log"
+export ARG_STORE="/tmp/btcpool-test-btc-argstore.conf"
+
+source $ARG_STORE 2>/dev/null
 
 echo -n >$STDOUT
+echo -n >$ARG_STORE
+
+# stored args
+if [ "x$APT_MIRROR_URL" != "x" ]; then
+    echo "export APT_MIRROR_URL='$APT_MIRROR_URL'" >> $ARG_STORE
+fi
 
 echo "View logs:"
 echo "    tail -F $STDOUT"
