8a007ae21940b747421f047ff68e3a0acb309b5c
SwimmingTiger <hu60.cn@gmail.com>
2019-06-27 14:40:12 +0800

    Autotest: Fix failed to start test before port is being listened; Remove unused sleep.

--- docker/btcpool/test/btc/helper/functions.sh
+++ docker/btcpool/test/btc/helper/functions.sh
@@ -2,12 +2,34 @@
 
 # Wait for the container to run
 # arg1: container name
+WAIT_FOR_CONTAINER() {
+    container_name="$1"
+    while ! docker inspect -f '{{.State.Running}}' "$container_name" &>/dev/null; do
+        sleep 1
+    done
+}
+
+# Wait for the port be listened then run a command
+# arg1: container name
 # arg2...: args of wait-for-it.sh
 WAIT_FOR_IT() {
     container_name="$1"
     shift 1
-    while ! docker inspect -f '{{.State.Running}}' "$container_name" &>/dev/null; do
+    WAIT_FOR_CONTAINER "$container_name"
+    docker exec "$container_name" /wait-for-it.sh "$@"
+}
+
+# Wait for the port be listened without creating a connection
+# Use this function to wait for the sserver listen port to avoid initial session id changes.
+# arg1: container name
+# arg2: port
+WAIT_FOR_PORT() {
+    container_name="$1"
+    port="$2"
+    WAIT_FOR_CONTAINER "$container_name"
+    while [ "$(docker exec "$container_name" netstat -lntp | grep ":$port" | wc -l)" = "0" ]; do
+        echo "waiting for port $port..."
         sleep 1
     done
-    docker exec "$container_name" /wait-for-it.sh "$@"
+    echo "port $port is ready"
 }
