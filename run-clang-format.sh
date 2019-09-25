52e53b0804e0f6687d3bd6998a71417333cc2c8c
Hanjiang Yu <42531996+de1acr0ix@users.noreply.github.com>
2019-09-25 09:54:36 +0800

    Remove tools folder from clang-format check

--- run-clang-format.sh
+++ run-clang-format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-for SRC_DIR in src test tools
+for SRC_DIR in src test
 do
   find $SRC_DIR -name '*.h' -or -name '*.inl' -or -name '*.cc' | xargs clang-format -i -style file
 done
