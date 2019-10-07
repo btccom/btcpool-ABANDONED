063981daa75f3ded26e47fc8a5e8bd5658186cb2
SwimmingTiger <hu60.cn@gmail.com>
2019-10-07 18:56:22 +0800

    run-manually.sh: Generate a block for bitcoind to prevent the getblocktemplate call from failing

--- docker/btcpool/test/btc/run-manually.sh
+++ docker/btcpool/test/btc/run-manually.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # automatic integration test for mining Bitcoin
 
+export BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
 export BASE_IMAGE=btccom/btcpool_build:btc-0.16.3
+
+# Generate a block for bitcoind to prevent the getblocktemplate call from failing
+export GENERATE_BLOCK=1
+
 source ../common/run-manually.sh
