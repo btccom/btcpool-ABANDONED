dcf23038b12f8499bd90e506c825fc05c246852d
Hanjiang Yu <42531996+de1acr0ix@users.noreply.github.com>
2020-08-16 18:35:34 +0800

    Use async/await

--- rust/nodebridge/src/ckb/rpc.rs
+++ rust/nodebridge/src/ckb/rpc.rs
@@ -1,5 +1,6 @@
 use ckb_jsonrpc_types::{Block, BlockTemplate};
 use ckb_types::H256;
+use futures::compat::Future01CompatExt;
 use futures::future::Future;
 use jsonrpc_core::Value;
 use jsonrpc_core_client::{RpcChannel, RpcError, TypedClient};
@@ -14,28 +15,19 @@ impl From<RpcChannel> for Client {
 }
 
 impl Client {
-    pub fn get_block_template(&self) -> impl Future<Item = BlockTemplate, Error = RpcError> {
+    pub fn get_block_template(&self) -> impl Future<Output = Result<BlockTemplate, RpcError>> {
         self.0
             .call_method("get_block_template", "BlockTemplate", Value::Null)
+            .compat()
     }
 
     pub fn submit_block(
         &self,
         work_id: u64,
         block: Block,
-    ) -> impl Future<Item = H256, Error = RpcError> {
+    ) -> impl Future<Output = Result<H256, RpcError>> {
         self.0
             .call_method("submit_block", "H256", (work_id.to_string(), block))
+            .compat()
     }
-
-    // pub fn get_cellbase_output_capacity_details(
-    //     &self,
-    //     hash: H256,
-    // ) -> impl Future<Item = Option<BlockReward>, Error = RpcError> {
-    //     self.0.call_method(
-    //         "get_cellbase_output_capacity_details",
-    //         "BlockReward",
-    //         (hash,),
-    //     )
-    // }
 }
