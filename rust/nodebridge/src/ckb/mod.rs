cf6ef7eae722ce8ce12444a6ae1483fb7b545942
jasper.li <jasper.li@bitmain.com>
2020-09-02 06:43:32 -0700

    revert Merge pull request #457

--- rust/nodebridge/src/ckb/mod.rs
+++ rust/nodebridge/src/ckb/mod.rs
@@ -1,32 +1,31 @@
-use std::str::from_utf8;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use std::{str, thread};
 
+use chrono::prelude::*;
 use ckb_jsonrpc_types::BlockTemplate;
-use ckb_types::prelude::*;
-use ckb_types::H256;
-use futures::compat::Future01CompatExt;
-use jsonrpc_core_client::transports::http;
+use ckb_types::prelude::{Builder, Entity, Pack};
+use ckb_types::{H256};
+use failure::Error;
+use futures::future::{loop_fn, Loop};
+use futures::sync::mpsc::{channel, Sender};
+use jsonrpc_core_client::{transports::http, RpcError};
 use log::*;
 use mysql_async::prelude::*;
 use rdkafka::consumer::{Consumer, StreamConsumer};
 use rdkafka::message::Message as KafkaMessage;
 use rdkafka::producer::{FutureProducer, FutureRecord};
-use rdkafka::topic_partition_list::Offset;
-use rdkafka::util::Timeout;
+use rdkafka::topic_partition_list::Offset::Offset;
 use rdkafka::{ClientConfig, TopicPartitionList};
-use tokio::runtime::Runtime;
-use tokio::spawn;
-use tokio::sync::mpsc::{Receiver, Sender};
-use tokio::time::delay_for;
+use tokio::prelude::*;
+use tokio::runtime::current_thread;
+use tokio::timer::Delay;
 
-use chrono::Utc;
-use futures::StreamExt;
+use job::{MiningJob, Repository, SolvedShare};
+
+use crate::ckb::rpc::Client;
 
 mod job;
 mod rpc;
-
-use job::*;
-use rpc::*;
 
 enum Message {
     BlockTemplate(BlockTemplate),
@@ -45,127 +44,112 @@ impl From<SolvedShare> for Message {
     }
 }
 
-fn consume_solved_shares(kafka_brokers: String, topic: String, mut tx: Sender<Message>) {
-    spawn(async move {
-        info!(
-            "Creating solved share consumer to Kafka broker {} topic {}",
-            kafka_brokers, topic
-        );
-        let consumer: StreamConsumer = ClientConfig::new()
-            .set("bootstrap.servers", &kafka_brokers)
-            .set("enable.auto.commit", "false")
-            .set("enable.partition.eof", "false")
-            .set("group.id", "nodebridge-ckb")
-            .set("message.timeout.ms", "5000")
-            .create()
-            .expect("Failed to solved share consumer");
+fn consume_solved_shares(kafka_brokers: &str, topic: &str, tx: Sender<Message>) {
+    info!(
+        "Creating solved share consumer to Kafka broker {} topic {}",
+        kafka_brokers, topic
+    );
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("bootstrap.servers", kafka_brokers)
+        .set("enable.auto.commit", "false")
+        .set("enable.partition.eof", "false")
+        .set("group.id", "nodebridge-ckb")
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Failed to solved share consumer");
 
-        let mut tpl = TopicPartitionList::new();
-        tpl.add_partition_offset(&topic, 0, Offset::Offset(-2003));
-        consumer
-            .assign(&tpl)
-            .expect("Failed to assign solved share topic");
+    let mut tpl = TopicPartitionList::new();
+    tpl.add_partition_offset(topic, 0, Offset(-2003));
+    consumer
+        .assign(&tpl)
+        .expect("Failed to assign solved share topic");
 
-        let mut stream = consumer.start();
-        while let Some(result) = stream.next().await {
-            match result {
-                Ok(msg) => match msg.detach().payload() {
-                    Some(payload) => {
-                        info!(
-                            "Solved share received: {}",
-                            from_utf8(payload).unwrap_or_default()
-                        );
-                        match serde_json::from_slice::<SolvedShare>(payload) {
-                            Ok(solved_share) => {
-                                tx.send(Message::from(solved_share))
-                                    .await
-                                    .unwrap_or_else(|e| {
-                                        error!("Failed to send solved share for processing: {}", e);
-                                    });
-                            }
-                            Err(e) => {
-                                error!("Failed to deserialize solved share: {}", e);
-                            }
-                        }
-                    }
-                    None => {
-                        error!("No payload for solved share message");
-                    }
-                },
-                Err(e) => {
-                    warn!("Error while receiving from Kafka: {:?}", e);
+    let task = consumer
+        .start()
+        .filter_map(|result| match result {
+            Ok(msg) => Some(msg),
+            Err(kafka_error) => {
+                warn!("Error while receiving from Kafka: {:?}", kafka_error);
+                None
+            }
+        })
+        .for_each(move |msg| {
+            let msg = msg.detach();
+            if let Some(payload) = msg.payload() {
+                if let Ok(solved_share) = serde_json::from_slice(payload) {
+                    info!(
+                        "Solved share received: {}",
+                        str::from_utf8(payload).unwrap_or_default()
+                    );
+                    let task = tx
+                        .clone()
+                        .send(Message::SolvedShare(solved_share))
+                        .and_then(|_| Ok(()))
+                        .map_err(|e| error!("Failed to consume solved share: {}", e));
+                    current_thread::spawn(task);
                 }
             }
-        }
-    });
+            Ok(())
+        });
+    current_thread::block_on_all(task).expect("Failed to start consuming solved shares");
 }
 
-fn produce_job(producer: FutureProducer, topic: String, job: MiningJob) {
-    match serde_json::to_string(&job) {
-        Ok(job_str) => {
-            spawn(async move {
-                info!("Producing mining job: {}", &job_str);
-                let record = FutureRecord::to(&topic)
-                    .key("")
-                    .partition(0)
-                    .payload(&job_str);
-                match producer.send(record, Timeout::Never).await {
-                    Ok((partition, offset)) => {
-                        info!(
-                            "Mining job sent to partition {} offset {}",
-                            partition, offset
-                        );
-                    }
-                    Err((e, _)) => {
-                        error!("Failed to produce mining job: {}", e);
-                    }
-                }
-            });
-        }
-        Err(e) => {
-            error!("Failed to serialize mining job: {}", e);
-        }
+fn produce_job(producer: &FutureProducer, topic: &str, job: &MiningJob) {
+    if let Ok(job_str) = serde_json::to_string(job) {
+        info!("Producing mining job: {}", &job_str);
+        let record = FutureRecord::to(topic)
+            .key("")
+            .partition(0)
+            .payload(&job_str);
+        let task = producer
+            .send(record, 0)
+            .and_then(|_| Ok(()))
+            .map_err(|e| error!("Failed to produce mining job: {}", e));
+        tokio::spawn(task);
     }
 }
 
-fn execute_query(url: String, stmt: String) {
-    spawn(async move {
-        let pool = mysql_async::Pool::new(url);
-        let delay = 5u64;
-        for count in 0..5 {
-            if count != 0 {
-                delay_for(Duration::from_secs(delay)).await;
-            }
-
-            match pool.get_conn().await {
-                Ok(mut conn) => {
-                    info!("Executing query: {}", &stmt);
-                    match conn.query_drop(&stmt).await {
-                        Ok(_) => {
-                            info!("Query is successfully executed");
-                            break;
-                        }
-                        Err(e) => {
-                            error!(
-                                "Failed to execute query: {}, retrying after {} seconds",
-                                e, delay
-                            );
+fn execute_query(url: &str, stmt: String) {
+    let pool = mysql_async::Pool::new(url);
+    let count = 1;
+    let task = loop_fn((count, pool, stmt), |(count, pool, stmt)| {
+        {
+            let pool = pool.clone();
+            let stmt = stmt.clone();
+            pool.get_conn().and_then(|conn| {
+                info!("Executing query: {}", &stmt);
+                conn.prep_exec(stmt, ())
+            })
+        }
+        .then(
+            move |result| -> Box<dyn Future<Item = _, Error = failure::Error> + Send> {
+                match result {
+                    Ok(_) => {
+                        info!("Query is successfully executed");
+                        pool.disconnect();
+                        Box::new(future::ok(Loop::Break(())))
+                    }
+                    Err(e) => {
+                        if count >= 5 {
+                            Box::new(future::err(Error::from(e)))
+                        } else {
+                            error!("Failed to execute query: {}, retrying", e);
+                            let fut = Delay::new(Instant::now() + Duration::from_secs(5))
+                                .and_then(move |_| Ok(Loop::Continue((count + 1, pool, stmt))))
+                                .map_err(|e| Error::from(e));
+                            Box::new(fut)
                         }
                     }
                 }
-                Err(e) => {
-                    error!(
-                        "Failed to get connection: {}, retrying after {} seconds",
-                        e, delay
-                    );
-                }
-            }
-        }
-    });
+            },
+        )
+    })
+    .map_err(|e| error!("Failed to execute query: {}", e));
+    tokio::spawn(task);
 }
 
 fn create_block_record(
-    url: String,
+    url: &str,
     user_id: i32,
     worker_id: i64,
     worker_name: &str,
@@ -192,7 +176,19 @@ fn create_block_record(
     execute_query(url, stmt);
 }
 
-fn submit_block(rpc_client: Client, job: MiningJob, solved_share: SolvedShare, db_url: String) {
+// fn update_block_reward(url: &str, block_hash: &H256, block_reward: &Result<u64, RpcError>) {
+//     let update = match block_reward {
+//         Ok(reward) => format!("`rewards` = {}", reward),
+//         Err(e) => format!("`rpc_error` = `{}`", e),
+//     };
+//     let stmt = format!(
+//         "UPDATE found_blocks SET {} WHERE `hash` = '0x{}'",
+//         &update, &block_hash,
+//     );
+//     execute_query(url, stmt);
+// }
+
+fn submit_block(rpc_client: Client, job: &MiningJob, solved_share: &SolvedShare, db_url: String) {
     let header = job
         .block
         .header()
@@ -209,7 +205,7 @@ fn submit_block(rpc_client: Client, job: MiningJob, solved_share: SolvedShare, d
     let hash = H256::from_slice(&block.hash().raw_data()).unwrap_or_default();
 
     create_block_record(
-        db_url,
+        &db_url,
         solved_share.user_id,
         solved_share.worker_id,
         &solved_share.worker_name,
@@ -221,68 +217,81 @@ fn submit_block(rpc_client: Client, job: MiningJob, solved_share: SolvedShare, d
         &job.compact_target,
     );
 
-    spawn(async move {
-        info!("Submitting block {}", &hash);
-        match rpc_client
-            .submit_block(job.work_id, block.data().into())
-            .await
-        {
+    info!("Submitting block {}", &hash);
+    let task = rpc_client
+        .submit_block(job.work_id, block.data().into())
+        .then(move |result| match result {
             Ok(hash_returned) => {
                 info!("Submit block {} returned {}", &hash, hash_returned);
+                // let fut = rpc_client
+                //     .get_cellbase_output_capacity_details(hash.clone())
+                //     .then(move |result| match result {
+                //         Ok(reward) => {
+                //             update_block_reward(
+                //                 &db_url,
+                //                 &hash,
+                //                 &Ok(reward.unwrap_or_default().total.into()),
+                //             );
+                //             Ok(())
+                //         }
+                //         Err(e) => {
+                //             update_block_reward(&db_url, &hash, &Err(e));
+                //             Err(())
+                //         }
+                //     });
+                // tokio::spawn(fut);
+                Ok(())
             }
             Err(e) => {
                 error!("Failed to submit block {}: {}", &hash, e);
+                // update_block_reward(&db_url, &hash, &Err(e));
+                Err(())
             }
-        }
-    });
+        });
+    tokio::spawn(task);
 }
 
 fn handle_messages(
-    kafka_brokers: String,
+    kafka_brokers: &str,
     job_topic: String,
     rpc_client: Client,
     db_url: String,
-    mut rx: Receiver<Message>,
-) {
-    spawn(async move {
-        info!(
-            "Creating job producer to Kafka broker {} topic {}",
-            &kafka_brokers, &job_topic
-        );
-        let producer: FutureProducer = ClientConfig::new()
-            .set("bootstrap.servers", &kafka_brokers)
-            .set("message.timeout.ms", "5000")
-            .create()
-            .expect("Failed to create job producer");
+) -> Sender<Message> {
+    info!(
+        "Creating job producer to Kafka broker {} topic {}",
+        kafka_brokers, &job_topic
+    );
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", kafka_brokers)
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Failed to create job producer");
 
-        let mut repository = Repository::new();
-        while let Some(msg) = rx.next().await {
-            match msg {
-                Message::BlockTemplate(block_template) => {
-                    produce_job(
-                        producer.clone(),
-                        job_topic.clone(),
-                        repository.on_block_template(block_template),
-                    );
-                }
-                Message::SolvedShare(solved_share) => {
-                    if let Some(job) = repository.on_solved_share(&solved_share) {
-                        submit_block(
-                            rpc_client.clone(),
-                            job.clone(),
-                            solved_share,
-                            db_url.clone(),
-                        );
-                    } else {
-                        error!(
-                            "Failed to find work for solved share {} nonce {}",
-                            solved_share.pow_hash, solved_share.nonce
-                        );
-                    }
-                }
+    let mut repository = Repository::new();
+    let (tx, rx) = channel::<Message>(256);
+    let task = rx.for_each(move |msg| match msg {
+        Message::BlockTemplate(block_template) => {
+            produce_job(
+                &producer,
+                &job_topic,
+                &repository.on_block_template(block_template),
+            );
+            Ok(())
+        }
+        Message::SolvedShare(solved_share) => {
+            if let Some(job) = repository.on_solved_share(&solved_share) {
+                submit_block(rpc_client.clone(), job, &solved_share, db_url.clone());
+            } else {
+                error!(
+                    "Failed to find work for solved share {} nonce {}",
+                    solved_share.pow_hash, solved_share.nonce
+                );
             }
+            Ok(())
         }
     });
+    tokio::spawn(task);
+    tx
 }
 
 pub fn run(
@@ -293,34 +302,40 @@ pub fn run(
     solved_share_topic: String,
     db_url: String,
 ) {
-    let mut runtime = Runtime::new().expect("Fail to create main runtime");
-    runtime.block_on(async {
-        info!("Connecting to CKB node {}", rpc_addr);
-        let result = http::connect::<Client>(rpc_addr).compat().await;
-        let rpc_client = result.expect("Failed to connect to CKB node");
-        let (mut tx, rx) = tokio::sync::mpsc::channel::<Message>(256);
-        handle_messages(
-            kafka_brokers.clone(),
-            job_topic.clone(),
-            rpc_client.clone(),
-            db_url.clone(),
-            rx,
-        );
-        consume_solved_shares(kafka_brokers, solved_share_topic, tx.clone());
-        loop {
-            match rpc_client.get_block_template().await {
-                Ok(block_template) => {
-                    tx.send(Message::from(block_template))
-                        .await
-                        .unwrap_or_else(|e| {
-                            error!("Failed to send block template for processing: {}", e)
-                        });
-                }
-                Err(e) => {
-                    error!("Failed to get block template: {}", e);
-                }
-            }
-            delay_for(Duration::from_millis(rpc_interval)).await;
-        }
-    });
+    info!("Connecting to CKB node {}", rpc_addr);
+    let fut = http::connect(rpc_addr)
+        .and_then(move |rpc_client: rpc::Client| {
+            let tx = handle_messages(&kafka_brokers, job_topic, rpc_client.clone(), db_url);
+            {
+                let tx = tx.clone();
+                thread::spawn(move || {
+                    consume_solved_shares(&kafka_brokers, &solved_share_topic, tx)
+                })
+            };
+
+            loop_fn(
+                (rpc_client, rpc_interval, tx),
+                |(rpc_client, rpc_interval, tx)| {
+                    rpc_client.get_block_template().and_then(move |result| {
+                        info!(
+                            "RPC get_block_template result: {}",
+                            serde_json::to_string(&result).unwrap_or_default()
+                        );
+
+                        let task = tx
+                            .clone()
+                            .send(Message::BlockTemplate(result))
+                            .and_then(|_| Ok(()))
+                            .map_err(|e| error!("Failed to consume block template: {}", e));
+                        tokio::spawn(task);
+
+                        Delay::new(Instant::now() + Duration::from_millis(rpc_interval))
+                            .and_then(move |_| Ok(Loop::Continue((rpc_client, rpc_interval, tx))))
+                            .map_err(|e| RpcError::Other(Error::from(e)))
+                    })
+                },
+            )
+        })
+        .map_err(|e| panic!("RPC client error: {}", e));
+    tokio::run(fut);
 }
--- rust/nodebridge/src/ckb/rpc.rs
+++ rust/nodebridge/src/ckb/rpc.rs
@@ -1,6 +1,5 @@
 use ckb_jsonrpc_types::{Block, BlockTemplate};
 use ckb_types::H256;
-use futures::compat::Future01CompatExt;
 use futures::future::Future;
 use jsonrpc_core::Value;
 use jsonrpc_core_client::{RpcChannel, RpcError, TypedClient};
@@ -15,19 +14,28 @@ impl From<RpcChannel> for Client {
 }
 
 impl Client {
-    pub fn get_block_template(&self) -> impl Future<Output = Result<BlockTemplate, RpcError>> {
+    pub fn get_block_template(&self) -> impl Future<Item = BlockTemplate, Error = RpcError> {
         self.0
             .call_method("get_block_template", "BlockTemplate", Value::Null)
-            .compat()
     }
 
     pub fn submit_block(
         &self,
         work_id: u64,
         block: Block,
-    ) -> impl Future<Output = Result<H256, RpcError>> {
+    ) -> impl Future<Item = H256, Error = RpcError> {
         self.0
             .call_method("submit_block", "H256", (work_id.to_string(), block))
-            .compat()
     }
+
+    // pub fn get_cellbase_output_capacity_details(
+    //     &self,
+    //     hash: H256,
+    // ) -> impl Future<Item = Option<BlockReward>, Error = RpcError> {
+    //     self.0.call_method(
+    //         "get_cellbase_output_capacity_details",
+    //         "BlockReward",
+    //         (hash,),
+    //     )
+    // }
 }
