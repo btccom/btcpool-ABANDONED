a0b564cda11efb9699917c7d3841c5455321c172
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-09 08:27:38 +0800

    Delete 3rdparty directory

--- 3rdparty/cuckoo/cuckaroo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include <cstddef>
-#include <vector>
-
-class siphash_keys;
-
-bool verify_cuckaroo(const std::vector<uint64_t> &edges, siphash_keys &keys, uint32_t edgeBits);
--- 3rdparty/cuckoo/cuckarooz.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include <cstddef>
-#include <vector>
-
-class siphash_keys;
-
-bool verify_cuckarooz(const std::vector<uint64_t> &edges, siphash_keys &keys, uint32_t edge_bits);
